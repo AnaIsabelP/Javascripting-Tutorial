ee39438588a3f02d09352492ef5d2d49b400db00
AnaIsabelP <100319653+AnaIsabelP@users.noreply.github.com>
2022-03-20 20:45:26 -0500

    Add files via upload

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+let fruit = 'oranje'
+fruit.length
+if (fruit > 5){
+    console.log('The fruit name has more than five characters.')
+}
+    else {
+        console.log('The fruit name has more than five characters.')
+    }
---
+++ number-to-string.js
@@ -0,0 +1,3 @@
+let n = '128'
+let result = n.toString()
+console.log(n)
